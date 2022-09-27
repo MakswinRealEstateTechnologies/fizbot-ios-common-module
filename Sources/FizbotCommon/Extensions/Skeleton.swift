6b9a19bb8c1e4b2bfc00b12145b59f1e537c3ac2
Alkın Çakıralar <a.cakiralar@gmail.com>
2022-09-27 10:16:34 +0300

    skeleton loading added

---
+++ Sources/FizbotCommon/Extensions/Skeleton.swift
@@ -0,0 +1,65 @@
+//
+//  Skeleton.swift
+//  
+//
+//  Created by Alkın Çakıralar on 27.09.2022.
+//
+
+import SwiftUI
+
+public struct Skeleton: ViewModifier {
+    @State private var phase: CGFloat = 0
+    var duration = 1.5
+    var bounce = false
+
+    public func body(content: Content) -> some View {
+        content
+            .redacted(reason: .placeholder)
+            .modifier(AnimatedMask(phase: phase).animation(
+                Animation.linear(duration: duration)
+                    .repeatForever(autoreverses: bounce)
+            ))
+            .onAppear { phase = 0.8 }
+    }
+
+    struct AnimatedMask: AnimatableModifier {
+        var phase: CGFloat = 0
+
+        var animatableData: CGFloat {
+            get { phase }
+            set { phase = newValue }
+        }
+
+        func body(content: Content) -> some View {
+            content
+                .mask(GradientMask(phase: phase).scaleEffect(3))
+        }
+    }
+
+    struct GradientMask: View {
+        let phase: CGFloat
+        let centerColor = Color.black
+        let edgeColor = Color.black.opacity(0.3)
+
+        var body: some View {
+            LinearGradient(gradient:
+                Gradient(stops: [
+                    .init(color: edgeColor, location: phase),
+                    .init(color: centerColor, location: phase + 0.1),
+                    .init(color: edgeColor, location: phase + 0.2)
+                ]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        }
+    }
+}
+
+public extension View {
+    @ViewBuilder func skeleton(
+        active: Bool = true, duration: Double = 2.5, bounce: Bool = false
+    ) -> some View {
+        if active {
+            modifier(Skeleton(duration: duration, bounce: bounce))
+        } else {
+            self
+        }
+    }
+}
