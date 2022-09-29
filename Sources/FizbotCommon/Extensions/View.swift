dfd1c3519831d26a4dfda42a893fc7d18de5481c
Alkın Çakıralar <a.cakiralar@gmail.com>
2022-09-29 12:32:39 +0300

    view extensions added

---
+++ Sources/FizbotCommon/Extensions/View.swift
@@ -0,0 +1,32 @@
+//
+//  View.swift
+//  
+//
+//  Created by Alkın Çakıralar on 29.09.2022.
+//
+
+import SwiftUI
+
+public extension View {
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+    
+    func onBackground(_ f: @escaping () -> Void) -> some View {
+        self.onReceive(
+            NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification),
+            perform: { _ in f() }
+        )
+    }
+    
+    func onForeground(_ f: @escaping () -> Void) -> some View {
+        self.onReceive(
+            NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification),
+            perform: { _ in f() }
+        )
+    }
+}
